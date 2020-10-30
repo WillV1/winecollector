8cd5c0394ac1814fbec1a5d76b5c03d30257370b
WillV1 <wmvanhook@gmail.com>
2020-10-30 13:59:27 -0400

    Add confirmation modal

--- winecollector_project/settings.py
+++ winecollector_project/settings.py
@@ -1,3 +1,5 @@
+from .secrets import *
+
 """
 Django settings for winecollector_project project.
 
@@ -12,6 +14,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '845do_via9^#%sk-s!3srny2d3@o+y+)n^sh#akgk-aq@k&1n_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
