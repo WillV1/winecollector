82060712bd3d34b2dadbb0b41607cc33098dcbd4
WillV1 <wmvanhook@gmail.com>
2020-10-30 14:23:27 -0400

    Update settings

--- winecollector_project/settings.py
+++ winecollector_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-SECRET_KEY = os.environ[‘secret_key’]
+SECRET_KEY = os.environ('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
