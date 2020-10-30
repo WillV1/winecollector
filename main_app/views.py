23cf3b93c89e50a073936d131e2678803f2435d6
WillV1 <wmvanhook@gmail.com>
2020-10-30 10:21:52 -0400

    Add authorization

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from .models import Producer, Distributor
 from .forms import WineForm, ProducerForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -32,19 +33,21 @@ def signup(request):
 
 #------PRODUCERS
 
+@login_required
 def producers_index(request):
     if request.method == 'POST':
         producer_form = ProducerForm(request.POST)
         if producer_form.is_valid():
-            new_producer = produceer_form.save(commit=False)
+            new_producer = producer_form.save(commit=False)
             new_producer.user = request.user
             new_producer.save()
-            return redirect('prodcuers_index')
-    producers = Producer.objects.all()
+            return redirect('producers_index')
+    producers = Producer.objects.filter(user=request.user)
     producer_form = ProducerForm()
     context = {'producers': producers, 'producer_form': producer_form}
     return render(request, 'producers/index.html', context)
 
+@login_required
 def producers_details(request, producer_id):
     producer = Producer.objects.get(id=producer_id)
     available_distributors = Distributor.objects.exclude(id__in = producer.distributors.all().values_list('id'))
@@ -55,11 +58,13 @@ def producers_details(request, producer_id):
         'distributors': available_distributors
         })
 
+@login_required
 def add_producer(request):
     if request.method == 'POST':
         producer_form = ProducerForm(request.POST)
         if producer_form.is_valid():
             new_producer = producer_form.save(commit=False)
+            new_producer.user = request.user
             new_producer.save()
             return redirect('producers_details', new_producer.id)
     else: 
@@ -67,11 +72,13 @@ def add_producer(request):
         context = {'form': form}
         return render(request, 'producers/new.html', context)
 
+@login_required
 def delete_producer(request, producer_id):
     Producer.objects.get(id=producer_id).delete()
 
     return redirect('producers_index')
 
+@login_required
 def edit_producer(request, producer_id):
     producer = Producer.objects.get(id=producer_id)
 
@@ -88,6 +95,7 @@ def edit_producer(request, producer_id):
 
 #-----ADD WINE
 
+@login_required
 def add_wine(request, producer_id):
     form = WineForm(request.POST)
 
@@ -100,11 +108,13 @@ def add_wine(request, producer_id):
 
 #------DISTRIBUTOR
 
+@login_required
 def assoc_distributor(request, producer_id, distributor_id):
     # Note that you can pass a toy's id instead of the whole object
     Producer.objects.get(id=producer_id).distributors.add(distributor_id)
     return redirect('producers_details', producer_id=producer_id)
 
+@login_required
 def remove_distributor(request, producer_id, distributor_id):
     Producer.objects.get(id=producer_id).distributors.remove(distributor_id)
     return redirect('producers_details', producer_id=producer_id)
